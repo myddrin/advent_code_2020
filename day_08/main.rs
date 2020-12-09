e7fe089f64c369badeb0d02e98172519a305b802
Thomas Richard <thomrich0@gmail.com>
2020-12-09 07:36:00 +0000

    Day 8 and 9

---
+++ day_08/main.rs
@@ -0,0 +1,176 @@
+use std::{io, env};
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+#[derive(Debug,PartialEq,Eq,Clone)]
+enum Op {
+    Nop,
+    Jmp,
+    Acc,
+}
+
+
+impl Op {
+    fn from_string(value: &str) -> Op {
+        use Op::*;
+        match value {
+            "nop" => Nop,
+            "jmp" => Jmp,
+            "acc" => Acc,
+            &_ => Nop,  // actually a bug
+        }
+    }
+}
+
+#[derive(Debug,Clone)]
+struct Instruction {
+    op: Op,
+    value: i32,
+}
+
+impl Instruction {
+    fn from_string(line: String) -> Option<Instruction> {
+        let entries: Vec<&str> = line.split_whitespace().collect();
+        let op = entries.get(0)?;
+        let value = entries.get(1)?.parse();
+        if value.is_err() {
+            return None;
+        }
+        Some(Instruction {
+            op: Op::from_string(op),
+            value: value.unwrap(),
+            // accumulator_when_visited: None,
+        })
+    }
+
+    fn read(path: &str) -> io::Result<Vec<Instruction>> {
+        let file = File::open(path)?;
+        let br = BufReader::new(file);
+        let mut rv = Vec::new();
+
+        for line in br.lines() {
+            let line = line?;
+            // TODO(tr) handle bad line to io::Error
+            let op = Self::from_string(line).unwrap();
+            rv.push(op);
+        }
+        Ok(rv)
+    }
+
+    fn execute(&self) -> (i32, i32) {
+        match self.op {
+            Op::Nop => (0, 1),
+            Op::Jmp => (0, self.value),
+            Op::Acc => (self.value, 1),
+        }
+    }
+
+    fn to_program(code: &[Instruction]) -> Vec<(&Instruction, Option<i32>)> {
+        let mut rv = Vec::new();
+        for op in code {
+            rv.push((op, None));
+        }
+        rv
+    }
+
+    fn fix_and_run(code: &[Instruction], change: usize) -> Option<i32> {
+        let mut code = code.clone().to_vec();
+        println!("[Trying to fix {} to nop]", change);
+        code[change].op = if code[change].op == Op::Jmp {
+            Op::Nop
+        } else {
+            Op::Jmp
+        };
+        let (last_acc, _last_cp, found_jmp) = Self::run(&code);
+        if found_jmp.is_none() {
+            Some(last_acc)
+        } else {
+            None
+        }
+    }
+
+    fn run(code: &[Instruction]) -> (i32, i32, Option<Vec<usize>>) {
+        let mut code = Self::to_program(code);
+        let mut accumulator = 0;
+        let mut code_pointer: i32 = 0;
+        let mut found_jump: Vec<usize> = Vec::new();
+
+        while code_pointer < code.len() as i32 {
+            let cp = code_pointer as usize;
+            if code[cp].0.op != Op::Acc {
+                found_jump.push(cp);
+            }
+            let (mod_acc, mod_cp) = code[cp].0.execute();
+
+            accumulator += mod_acc;
+            code[cp].1 = Some(accumulator);
+            code_pointer += mod_cp;
+
+
+            // do it before finishing so we can return the offending CP
+            let next = code.get(code_pointer as usize);
+            if let Some(next) = next {
+                if next.1.is_some() {
+                    eprintln!("{:?} loop!, found {} jumps", code[cp], found_jump.len());
+                    return (accumulator, cp as i32, Some(found_jump));  // last accumulator value
+                }
+            } else if code_pointer < 0 {
+                eprintln!("{:?} neg cp!, found {} jumps", code[cp], found_jump.len());
+                return (accumulator, cp as i32, Some(found_jump));  // last accumulator value
+            }
+        }
+
+        eprintln!("Finished");
+        (accumulator, code_pointer, None)
+    }
+}
+
+fn main() {
+    let path = env::args().nth(1).expect("please supply a path");
+    let contents = Instruction::read(&path).expect("no content");
+
+    println!("Loaded {} lines of code", contents.len());
+    // eprintln!("{:?}", contents);
+
+    let (last_acc, last_cp, jmps) = Instruction::run(&contents);
+    println!("Last accumulator: {} (cp={})", last_acc, last_cp);
+
+    if let Some(jmps) = jmps {
+        for jmp_offset in jmps {
+            let rv = Instruction::fix_and_run(&contents, jmp_offset);
+            if let Some(rv) = rv {
+                println!("Fix the progam at {} and the accumulator is {}", jmp_offset, rv);
+                break;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest(path, exp_last_acc, exp_last_cp,
+    case(&"day_08/test_1.txt", 5, 4),
+    )]
+    fn test_find_loop(path: &str, exp_last_acc: i32, exp_last_cp: i32) {
+        let contents = Instruction::read(&path);
+        assert!(contents.is_ok());
+        let contents = contents.unwrap();
+        let (last_acc, last_cp, _jmps) = Instruction::run(&contents);
+        assert_eq!(last_acc, exp_last_acc);
+        assert_eq!(last_cp, exp_last_cp);
+    }
+
+    #[rstest(path, fix_at, exp_last_acc,
+    case(&"day_08/test_1.txt", 7, Some(8)),
+    case(&"day_08/test_1.txt", 2, None),
+    )]
+    fn test_fix_and_run(path: &str, fix_at: usize, exp_last_acc: Option<i32>) {
+        let contents = Instruction::read(&path);
+        assert!(contents.is_ok());
+        let contents = contents.unwrap();
+        assert_eq!(Instruction::fix_and_run(&contents, fix_at), exp_last_acc);
+    }
+}
---
+++ day_09/main.rs
@@ -0,0 +1,125 @@
+use std::{io, env};
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+fn read(path: &str) -> io::Result<Vec<u64>> {
+    let file = File::open(path)?;
+    let br = BufReader::new(file);
+    let mut rv = Vec::new();
+
+    for line in br.lines() {
+        let line = line?;
+        let v = line.parse().unwrap();
+        rv.push(v);
+    }
+    Ok(rv)
+}
+
+fn is_valid(history: &[u64], value: u64) -> bool {
+    // brute force check, we may want something a bit more clever if we can.
+    for (idx, a) in history.iter().enumerate() {
+        for (_j, b) in history.iter().enumerate().filter(|&(i, _)| i > idx) {
+            if a + b == value {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn first_invalid(values: &[u64], look_back: usize) -> Option<u64> {
+    let mut history: Vec<u64> = Vec::new();
+
+    for v in values {
+        if history.len() < look_back {
+            history.push(*v);
+        } else {
+            if !is_valid(&history, *v) {
+                return Some(v.clone());
+            }
+            history.remove(0);
+            history.push(*v);
+        }
+    }
+    None
+}
+
+fn has_weakness(values: &[u64], invalid: u64, size: usize) -> Option<Vec<u64>> {
+    let mut rv = Vec::new();
+
+    for v in values {
+        if rv.len() < size {
+            rv.push(*v);
+        } else {
+            let sum: u64 = rv.iter().sum();
+            if sum == invalid {
+                return Some(rv);
+            }
+            rv.remove(0);
+            rv.push(*v);
+        }
+    }
+    None
+}
+
+fn search_weakness(values: &[u64], invalid: u64) -> Option<Vec<u64>> {
+    for size in 2..values.len() {
+        if size % 10 == 0 {
+            eprintln!("Scanning for size {}", size);
+        }
+        let rv = has_weakness(values, invalid, size);
+        if rv.is_some() {
+            return rv;
+        }
+    }
+    None
+}
+
+fn main() {
+    let path = env::args().nth(1).expect("please supply a path");
+    let contents = read(&path).expect("no content");
+
+    let invalid = first_invalid(&contents, 25);
+    if let Some(invalid) = invalid {
+        println!("Q1: first invalid entry: {}", invalid);
+
+        let weakness = search_weakness(&contents, invalid);
+        if let Some(weakness) = weakness {
+            let min = weakness.iter().min().unwrap();
+            let max = weakness.iter().max().unwrap();
+            println!("Q2: smallest {} and largest {}: {}",
+                min,
+                max,
+                min + max
+            );
+        } else {
+            println!("No weakness found");
+        }
+    } else {
+        println!("Valid XMAS");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest(input, exp,
+    case(&[35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576,], Some(127))
+    )]
+    fn test_lookback_of_5(input: &[u64], exp: Option<u64>) {
+        assert_eq!(first_invalid(input, 5), exp);
+    }
+
+    #[rstest(input, weakness, exp_min, exp_max,
+    case(&[35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576,], 127, 15, 47),
+    )]
+    fn test_search_weakness(input: &[u64], weakness: u64, exp_min: u64, exp_max: u64) {
+        let rv = search_weakness(input, weakness);
+        assert!(rv.is_some());
+        let rv = rv.unwrap();
+        assert_eq!(*rv.iter().min().unwrap(), exp_min);
+        assert_eq!(*rv.iter().max().unwrap(), exp_max);
+    }
+}
