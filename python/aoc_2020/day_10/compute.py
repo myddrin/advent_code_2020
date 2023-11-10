fd0e2030f81beaff79213a812e62334275058801
Thomas Richard <thomas.richard@farfetch.com>
2023-11-10 12:40:06 +0000

    q2 brute force: not fast enough

--- python/aoc_2020/day_10/compute.py
+++ python/aoc_2020/day_10/compute.py
@@ -2,6 +2,7 @@ import dataclasses
 from typing import (
     Dict,
     List,
+    Set,
 )
 
 from aoc_2020 import BaseRunner
@@ -42,6 +43,26 @@ class Adapters:
 
         return diffs
 
+    def _check_chain_brute(self, current: List, left: Set[int]) -> int:
+        add = []
+        last = current[-1]
+
+        for d in range(1, 4):
+            potential_next = last + d
+            if potential_next in left:
+                add.append(self._check_chain_brute(current + [potential_next], left - {potential_next}))
+
+        if not add and last - self.device_joltage <= 3:
+            add.append(1)
+
+        return sum(add)
+
+    def count_permutations(self) -> int:
+        return self._check_chain_brute(
+            [0],
+            set(self.original),
+        )
+
 
 @BaseRunner.register
 class Day10(BaseRunner):
@@ -53,11 +74,16 @@ class Day10(BaseRunner):
         return differences[1] * differences[3]
 
     @classmethod
+    def q2(cls, data: Adapters) -> int:
+        return data.count_permutations()
+
+    @classmethod
     def compute(cls, cli_args):
 
         data = Adapters.from_file(cli_args.input)
 
         print(f'Q1 answer is {cls.q1(data)}')
+        print(f'Q2 answer is {cls.q2(data)}')
 
 
 if __name__ == '__main__':
--- python/aoc_2020/day_10/test_compute.py
+++ python/aoc_2020/day_10/test_compute.py
@@ -54,6 +54,14 @@ class TestAdapters:
             3: 5,
         }
 
+    def test_count_permutations_tiny(self):
+        obj = Adapters(sorted(tiny_example))
+        assert obj.count_permutations() == 8
+
+    def test_count_permutations_small(self, small_ex_txt):
+        obj = Adapters.from_file(small_ex_txt)
+        assert obj.count_permutations() == 19208
+
     def test_small_ex(self, small_ex_txt):
         obj = Adapters.from_file(small_ex_txt)
         assert obj.device_joltage == 52
@@ -64,6 +72,14 @@ class TestAdapters:
             3: 10,
         }
 
+    def test_input_has_no_duplicates(self, input_txt):
+        obj = Adapters.from_file(input_txt)
+        assert obj.compute_differences()[0] == 0
+
     def test_q1(self, input_txt):
         obj = Adapters.from_file(input_txt)
         assert Day10.q1(obj) == 2080
+
+    # def test_q2(self, input_txt):
+    #     obj = Adapters.from_file(input_txt)
+    #     assert Day10.q2(obj) == 6908379398144
